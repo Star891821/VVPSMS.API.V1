82b19564927b6f2290e60d7e7fc948a726493ed8
Star891821 <Star891821@gmail.com>
2023-09-06 16:35:11 +0530

    Interface File Checkin

---
+++ VVPSMS.Service/Repository/IAdmissionDocumentService.cs
@@ -0,0 +1,10 @@
+﻿using VVPSMS.Api.Models.ModelsDto;
+using VVPSMS.Domain.Models;
+
+namespace VVPSMS.Service.Repository
+{
+    public interface IAdmissionDocumentService
+    {
+        List<AdmissionDocumentDto> GetAllDocumentsById(int formId);
+    }
+}
