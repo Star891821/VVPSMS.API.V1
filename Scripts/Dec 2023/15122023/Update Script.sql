cd89ede877a518c217e3d725015d455b75b7db0b
Star891821 <Star891821@gmail.com>
2023-12-15 14:43:40 +0530

    Admission  Status Description Update Script

---
+++ Scripts/Dec 2023/15122023/Update Script.sql
@@ -0,0 +1,43 @@
+USE [VVPSMSDB]
+GO
+
+UPDATE [dbo].[MstAdmissionStatus]
+   SET 
+      [status_description] = 'Registrered'
+ WHERE status_id = 1
+GO
+UPDATE [dbo].[MstAdmissionStatus]
+   SET 
+      [status_description] = 'Submitted'
+ WHERE status_id = 2
+GO
+UPDATE [dbo].[MstAdmissionStatus]
+   SET 
+      [status_description] = 'Active'
+ WHERE status_id = 3
+GO
+UPDATE [dbo].[MstAdmissionStatus]
+   SET 
+      [status_description] = 'Reviewed'
+ WHERE status_id = 5
+GO
+UPDATE [dbo].[MstAdmissionStatus]
+   SET 
+      [status_description] = 'Entrance'
+ WHERE status_id = 6
+GO
+UPDATE [dbo].[MstAdmissionStatus]
+   SET 
+      [status_description] = 'Interview'
+ WHERE status_id = 7
+GO
+UPDATE [dbo].[MstAdmissionStatus]
+   SET 
+      [status_description] = 'Confirmed'
+ WHERE status_id = 8
+GO
+UPDATE [dbo].[MstAdmissionStatus]
+   SET 
+      [status_description] = 'Rejected'
+ WHERE status_id = 9
+GO
