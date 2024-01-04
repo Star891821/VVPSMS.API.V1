d3d3a715192563f57bae7469948f257e9a303b3a
siva kumar <siva.kumar@sustainedgeconsulting.com>
2024-01-04 16:50:32 +0530

    removed test

--- VVPSMS.API/Controllers/AuthController.cs
+++ VVPSMS.API/Controllers/AuthController.cs
@@ -22,13 +22,7 @@ namespace VVPSMS.API.Controllers
             _dataRepository = dataRepository;
             _configuration = configuration;
         }
-        [AllowAnonymous]
-        [Microsoft.AspNetCore.Mvc.HttpPost("AuthTest")]
-        public async Task<IActionResult> AuthTest()
-        {
-            
-            return Ok("Hello");
-        }
+       
         [AllowAnonymous]
         [Microsoft.AspNetCore.Mvc.HttpPost("Auth")]
         public async Task<IActionResult> Auth(LoginRequestDto loginRequest)
